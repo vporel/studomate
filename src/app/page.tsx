17d5c4745869f93502bde5cb0cacddf2413a1bf6
NKOUANANG KEPSEU VIVIAN p2513938 <p2513938@univ-lyon1.fr>
2025-09-17 14:41:21 +0200

    start reorganization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,34 +7,38 @@ import MenuBar from "@/components/MenuBar";
 import StatusBar from "@/components/StatusBar";
 import { Position } from "@xyflow/react";
 import GrafcetToolbarDnDContext, { GrafcetToolbarDnDProvider } from "@/components/grafcet/toolbar/GrafcetToolbarDnDContext";
+import { ProjectContextProvider } from "@/components/project/ProjectContext";
+import { PagesContextProvider } from "@/PagesContext";
 
-export default function Home() {
-	return (
-		<div style={{
-			display: "flex", flexDirection: "column",
-			height: "100vh", overflow: "hidden",
-		}}>
-			<MenuBar />
-			<Box sx={{
-				width: "100%",
-				display: "flex", flex: 1,
-				position: "relative", overflow: "hidden",
+export default function App() {
+	return <ProjectContextProvider>
+        <PagesContextProvider>
+			<div style={{
+				display: "flex", flexDirection: "column",
+				height: "100vh", overflow: "hidden",
 			}}>
-				<GrafcetToolbarDnDProvider>
-					<GrafcetToolbar />
-					<Box className="pages__container" sx={{
-						height: "100%", display: "flex", flex: 1, flexDirection: "column",
-						position: "relative",
-					}}>
-						<PagesTabBar tabsData={[
-							{id: "xx", title: "Grafcet 1", active: true},
-							{id: "yy", title: "Grafcet 2"},
-						]}/>
-						<GrafcetPage pageId="page-1"/>
-					</Box>
-				</GrafcetToolbarDnDProvider>
-			</Box>
-			<StatusBar />
-		</div>
-	);
+				<MenuBar />
+				<Box sx={{
+					width: "100%",
+					display: "flex", flex: 1,
+					position: "relative", overflow: "hidden",
+				}}>
+					<GrafcetToolbarDnDProvider>
+						<GrafcetToolbar />
+						<Box className="pages__container" sx={{
+							height: "100%", display: "flex", flex: 1, flexDirection: "column",
+							position: "relative",
+						}}>
+							<PagesTabBar tabsData={[
+								{id: "xx", title: "Grafcet 1", active: true},
+								{id: "yy", title: "Grafcet 2"},
+							]}/>
+							<GrafcetPage pageId="page-1"/>
+						</Box>
+					</GrafcetToolbarDnDProvider>
+				</Box>
+				<StatusBar />
+			</div>
+		</PagesContextProvider>
+	</ProjectContextProvider>
 }
